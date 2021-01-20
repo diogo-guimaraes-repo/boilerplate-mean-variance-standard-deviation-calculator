f0c3d0d695d2cf1340141e653e608a07f0241801
diogoguimaraesr <diogo.m.guimaraes.92@gmail.com>
2021-01-20 13:50:15 +0000

    calculations implementation

--- mean_var_std.py
+++ mean_var_std.py
@@ -2,7 +2,18 @@ import numpy as np
 
 def calculate(list):
 
+    operations = {"mean": np.mean, "variance": np.var, "standard deviation" : np.std, "max": np.max, "min": np.min, "sum":np.sum}
 
+    results = [ [0] * 3 for i in range(len(operations)) ]
 
+    calculations = dict()
+
+    matrix = np.array(list)
+    matrix = matrix.reshape(3,3)
+
+    for i, operation in enumerate(operations.keys()):
+      results[i] = [operations[operation](matrix, axis=0).tolist(), operations[operation](matrix, axis=1).tolist(), [operations[operation](matrix)]]
+
+      calculations[operation] = results[i]
 
     return calculations
