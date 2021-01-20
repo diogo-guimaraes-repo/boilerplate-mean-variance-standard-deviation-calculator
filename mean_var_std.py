38445081e1a29c905100805d11f24156794b4cf7
diogoguimaraesr <diogo.m.guimaraes.92@gmail.com>
2021-01-20 14:28:40 +0000

    implementation complete

--- mean_var_std.py
+++ mean_var_std.py
@@ -2,18 +2,29 @@ import numpy as np
 
 def calculate(list):
 
-    operations = {"mean": np.mean, "variance": np.var, "standard deviation" : np.std, "max": np.max, "min": np.min, "sum":np.sum}
+    if len(list) == 9:
+      operations = {
+        "mean": np.mean,
+        "variance": np.var,
+        "standard deviation" : np.std,
+        "max": np.max,
+        "min": np.min,
+        "sum":np.sum
+        }
 
-    results = [ [0] * 3 for i in range(len(operations)) ]
+      results = [ [0] * 3 for i in range(len(operations)) ]
+    
+      calculations = dict()
 
-    calculations = dict()
+      matrix = np.array(list)
+      matrix = matrix.reshape(3,3)
 
-    matrix = np.array(list)
-    matrix = matrix.reshape(3,3)
+      for i, operation in enumerate(operations.keys()):
+        results[i] = [operations[operation](matrix, axis=0).tolist(), operations[operation](matrix, axis=1).tolist(), operations[operation](matrix)]
 
-    for i, operation in enumerate(operations.keys()):
-      results[i] = [operations[operation](matrix, axis=0).tolist(), operations[operation](matrix, axis=1).tolist(), [operations[operation](matrix)]]
+        calculations[operation] = results[i]
 
-      calculations[operation] = results[i]
+    else:  
+      raise ValueError('List must contain nine numbers.')
 
     return calculations
